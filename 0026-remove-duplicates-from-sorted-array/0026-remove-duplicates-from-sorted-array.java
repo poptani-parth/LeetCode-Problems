8eba66955dc70ab8f5e30303eb65378fc78722f1
poptani-parth <poptaniparth809@gmail.com>
2026-07-02 14:18:11 +0530

    Time: 118 ms (3.81%), Space: 46.6 MB (62.39%) - LeetHub

---
+++ 0026-remove-duplicates-from-sorted-array/0026-remove-duplicates-from-sorted-array.java
@@ -0,0 +1,13 @@
+class Solution {
+    public int removeDuplicates(int[] nums) {
+        HashSet<Integer> set = new HashSet<>();
+        int index = 0;
+        for (int i = 0; i < nums.length; i++){
+            if(!set.contains(nums[i])){
+                set.add(nums[i]);
+                nums[index++] = nums[i];
+            }
+        }
+        return index;
+    }
+}

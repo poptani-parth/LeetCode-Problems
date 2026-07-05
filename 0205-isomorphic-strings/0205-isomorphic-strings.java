df1b6d7478991a3939d5a4ee42b62bd0b01f10b3
poptani-parth <poptaniparth809@gmail.com>
2026-07-05 11:15:55 +0530

    Time: 18 ms (25.83%), Space: 43.7 MB (86.35%) - LeetHub

--- 0205-isomorphic-strings/0205-isomorphic-strings.java
+++ 0205-isomorphic-strings/0205-isomorphic-strings.java
@@ -1,8 +1,5 @@
 class Solution {
     public boolean isIsomorphic(String s, String t) {
-        if(s.length() != t.length()){
-            return false;
-        }
         Map<Character, Character> map = new HashMap<>();
         for(int i = 0; i < s.length(); i++){
             char ss = s.charAt(i);

f82e0d28e7f174fdfb9d4ce7e74b70ca04500de6
poptani-parth <poptaniparth809@gmail.com>
2026-07-08 16:19:40 +0530

    Time: 11 ms (58.51%), Space: 46.3 MB (59.99%) - LeetHub

--- 0482-license-key-formatting/0482-license-key-formatting.java
+++ 0482-license-key-formatting/0482-license-key-formatting.java
@@ -1,24 +1,15 @@
 class Solution {
     public String licenseKeyFormatting(String s, int k) {
-        String temp = "", ans ="";
-        int i = s.length()-1;
-        while(i >= 0){
-            char c = s.charAt(i);
-            if(c != '-'){
-                temp += Character.toUpperCase(c);
+        StringBuilder sb = new StringBuilder();
+        for (int i = s.length()-1; i>=0; i--){
+            char ch = s.charAt(i);
+            if(ch != '-'){
+                if(sb.length() % (k + 1) == k){
+                    sb.append('-');
+                }   
+                sb.append(Character.toUpperCase(ch));
             }
-            i--;
         }
-        int c = k;
-        for(int j = 0; j < temp.length(); j++){
-            char ch = temp.charAt(j);
-            if(c == 0){
-                c = k;
-                ans = '-' + ans;
-            }
-            ans = ch + ans;
-            c--;
-        }
-        return ans;
+        return sb.reverse().toString();
     }
 }

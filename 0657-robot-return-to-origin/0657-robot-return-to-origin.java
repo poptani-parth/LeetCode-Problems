94c1ab6f3a03a91a4d6f60f0d391fcc9023aedaf
poptani-parth <poptaniparth809@gmail.com>
2026-07-11 16:18:46 +0530

    Time: 5 ms (69.11%), Space: 46.3 MB (10.09%) - LeetHub

---
+++ 0657-robot-return-to-origin/0657-robot-return-to-origin.java
@@ -0,0 +1,12 @@
+class Solution {
+    public boolean judgeCircle(String moves) {
+        int x = 0, y = 0;
+        for(char c : moves.toCharArray()){
+            if(c == 'U')    x++;
+            else if(c == 'D')   x--;
+            else if(c == 'L')   y++;
+            else if(c == 'R')   y--;
+        }
+        return (x == 0 && y == 0);
+    }
+}
